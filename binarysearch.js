cfe26e9f92bebd4d38654549a42eb8c8b7a49118
sameeranamarnath <85400557+sameeranamarnath@users.noreply.github.com>
2022-02-26 23:10:50 +0530

    Create binarysearch.js

---
+++ binarysearch.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+  let leftIndex=0;
+  let rightIndex=nums.length-1;
+    
+  while(leftIndex<=rightIndex)
+      {
+       
+       let middle= (leftIndex+rightIndex)>>1;
+        if(target<nums[middle])
+            {
+                rightIndex=middle-1;
+            }
+          
+         else if(target>nums[middle])
+             {
+                 leftIndex=middle+1;
+             }
+        
+          else
+              {
+                  return middle;
+              }
+      }
+    
+     return -1;
+
+};
